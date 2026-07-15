15e252b0e2d345bbe7b7245b42ed3e49e17601c1
kbui07 <kienhangbui@gmail.com>
2026-07-15 22:18:02 +0700

    Delete ss17/bth1 directory

--- ss17/bth1/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from database import Base, engine
-import models
-
-app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
--- ss17/bth1/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .warehouse import WarehouseModel
-from .package import PackageModel
-from .waybill import WaybillModel
-from .truck import TruckModel
-from .association import package_truck
